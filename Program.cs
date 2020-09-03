3306d4ea0a5259baf3553cb0ce635a07167586b9
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-09-02 23:30:54 -0300

    Add files via upload
    Exercício 24-08-2020

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Informe as medidas dos lados do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe as medidas dos lados do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
+            Console.WriteLine("A área do triângulo X é: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("A área do triângulo Y é: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("O triângulo X tem a maior área");
+            }
+            else
+            {
+                Console.WriteLine("O triângulo Y tem a maior área");
+            }
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Course	//Precisa ter o mesmo nome do programa principal
+{
+	class Triangulo
+	{
+		//Definição das variáveis, atributos 
+		public double A;
+		public double B;
+		public double C;
+
+		//Método de cálculo
+		public double Area()
+		{
+			double p = (A + B + C) / 2;
+			double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+			return raiz;
+		}
+	}
+}
+
+
